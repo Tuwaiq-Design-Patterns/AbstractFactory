adf7cc764dace2c7c0d49ddcd339a64d8f535b39
Nada Al-Otaibi <Nada.AlOtaibi@tuwaiq.edu.sa>
2021-06-07 14:02:55 +0300

    factory

---
+++ Factory/Factory/Program.cs
@@ -0,0 +1,56 @@
+﻿
+// Code on abstract factory desgin pattren
+using System;
+
+namespace Factory
+{
+    class Program
+    {
+        interface Chair
+        {
+            public void hasLegs();
+            public void sitOn();
+        }
+        public class VictorianChair : Chair
+        {
+
+            public void hasLegs()
+            {
+                Console.WriteLine("has four legs");
+            }
+            public void sitOn()
+            {
+                Console.WriteLine("You can");
+
+            }
+        }
+
+
+        public class ModrenChair : Chair
+        {
+
+            public void hasLegs()
+            {
+                Console.WriteLine("has 3 legs");
+
+            }
+            public void sitOn()
+            {
+                Console.WriteLine("You can");
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            ModrenChair mod = new ModrenChair();
+            mod.hasLegs();
+            mod.sitOn();
+            VictorianChair vic = new VictorianChair();
+            vic.hasLegs();
+            vic.sitOn();
+
+        }
+    }
+    }
