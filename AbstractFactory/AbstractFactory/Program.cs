9a7ed8275b1fcbfca5125c1414f9e645d659668d
Batool-Alghamdi <batool.alghamdi@tuwaiq.edu.sa>
2021-06-08 20:55:22 +0300

    skincare

---
+++ AbstractFactory/AbstractFactory/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+class Program
+{
+    public interface SkinCareFactory
+    {
+        IToner createToner();
+
+        ICleanser createCleanser();
+
+    }
+    public interface ICleanser
+    {
+        string txtCleanser();
+    }
+    public interface IToner
+    {
+        string txtToner();
+    }
+    public class CosrxFactory : SkinCareFactory
+    {
+        public IToner createToner()
+        {
+            return new Toner();
+        }
+
+        public ICleanser createCleanser()
+        {
+            return new Cleanser();
+        }
+
+    }
+    public class GlowFactory : SkinCareFactory
+    {
+        public IToner createToner()
+        {
+            return new Toner();
+        }
+
+        public ICleanser createCleanser()
+        {
+            return new Cleanser();
+        }
+    }
+
+    public class Toner : IToner
+    {
+
+
+        public string txtToner()
+        {
+            return "Toner: Use me daily after cleansing!";
+        }
+    }
+    public class Cleanser : ICleanser
+    {
+        public string txtCleanser()
+        {
+            return "Cleanser: Use me twice daily";
+        }
+    }
+
+
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("COSRX PRODUCTS");
+        ClientMethod(new CosrxFactory());
+        Console.WriteLine("Glow PRODUCTS");
+        ClientMethod(new GlowFactory());
+
+
+    }
+    public static void ClientMethod(SkinCareFactory SCF)
+    {
+        var pr = SCF.createCleanser();
+        var pr2 = SCF.createToner();
+        Console.WriteLine(pr.txtCleanser());
+        Console.WriteLine(pr2.txtToner());
+    }
+}
