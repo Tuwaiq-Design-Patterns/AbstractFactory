55123657d4d94dd52932ca99549db92f3e94dc6f
1Riyad <Riyad.Alghamdi@tuwaiq.edu.sa>
2021-06-08 13:48:56 -0400

    initial commit

---
+++ programmingLanguages/Factories.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace programmingLanguages
+{
+    /*The abstract factory interface*/
+    public interface IProgrammingLanguage
+    {   
+
+        IVariable CodeVariable();
+        IMethod CodeMethod();
+        IClass CodeClass();
+        ILambda CodeLambda();
+    }
+
+
+    /*The concrete factories*/
+
+    class CSharpLanguage : IProgrammingLanguage
+    {
+        public IVariable CodeVariable()
+        {
+            return new CSharpVariable();
+        }
+
+        public IMethod CodeMethod()
+        {
+            return new CSharpMethod();
+        }
+
+        public IClass CodeClass()
+        {
+            return new CSharpClass();
+        }
+
+        public ILambda CodeLambda()
+        {
+            return new CSharpLambda();
+        }
+    }
+
+    // ------------------------------------------------------
+    class PythonLanguage : IProgrammingLanguage
+    {
+        public IVariable CodeVariable()
+        {
+            return new PythonVariable();
+        }
+
+        public IMethod CodeMethod()
+        {
+            return new PythonMethod();
+        }
+
+        public IClass CodeClass()
+        {
+            return new PythonClass();
+        }
+
+        public ILambda CodeLambda()
+        {
+            return new PythonLambda();
+        }
+    }
+}
---
+++ programmingLanguages/Products.cs
@@ -0,0 +1,96 @@
+﻿using System;
+namespace programmingLanguages
+{
+
+    /* Products with thiers Concrete Products*/
+
+    public interface IVariable
+    {
+        string BuildVariable();
+    }
+
+    class CSharpVariable : IVariable
+    {
+        public string BuildVariable()
+        {
+            return "..CSharp variable.. |ex: var variable = value; ";
+        }
+    }
+
+    class PythonVariable : IVariable
+    {
+        public string BuildVariable()
+        {
+            return "..Python variable.. |ex: variable = value ";
+        }
+    }
+
+    // ------------------------------------------------------
+    public interface IMethod
+    {
+        string BuildMethod();
+    }
+
+    class CSharpMethod : IMethod
+    {
+        public string BuildMethod()
+        {
+            return "..CSharp Method.... |ex: void Method(){}";
+        }
+    }
+
+    class PythonMethod : IMethod
+    {
+        public string BuildMethod()
+        {
+            return "..Python Method.... |ex: def void Method(){}";
+        }
+    }
+
+
+    // ------------------------------------------------------
+    public interface IClass
+    {
+        string BuildClass();
+    }
+
+    class CSharpClass : IClass
+    {
+        public string BuildClass()
+        {
+            return "..CSharp Class..... |ex: class ClassName{}";
+        }
+    }
+
+    class PythonClass : IClass
+    {
+        public string BuildClass()
+        {
+            return "..Python Class..... |ex: class ClassName(){}";
+        }
+    }
+    // ------------------------------------------------------
+
+    public interface ILambda
+    {
+        string BuildLambda();
+    }
+
+
+    class CSharpLambda : ILambda
+    {
+        public string BuildLambda()
+        {
+            return "..CSharp Lambda.... |ex: lambda arguments : expression";
+        }
+    }
+
+
+    class PythonLambda : ILambda
+    {
+        public string BuildLambda()
+        {
+            return "..Python Lambda.... |ex: (parameters) => expression";
+        }
+    }
+}
